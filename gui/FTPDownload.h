5d90cf00c1403b61eb814c87a29f60f2f0ea1ac4
LittleKey <LittleKeyRain@gmail.com>
2014-05-04 00:46:33 +0800

    update GUI communication's method...

--- gui/FTPDownload.h
+++ gui/FTPDownload.h
@@ -1,17 +1,24 @@
 #include <QProcess>
 #include <QString>
+#include <QStringList>
 
 class FTPDownload
 {
     public:
-        FTPDownload(QString const filename);
+        FTPDownload(QString const programName);
         ~FTPDownload();
 
+        // 对程序做标准输入输出
         void Input(QString const context);
         QString const Output(void);
-        bool const Run(void);
+        // 同上，不过会阻塞
+        void BlockInput(QString const context);
+        QString const BlockOutput(void);
+        // 提供参数并运行程序
+        bool const Run(QStringList const arguments);
 
     private:
-        QString const filename;
+        QString const programName;
+        QStringList const args;
         QProcess* program;
 };
