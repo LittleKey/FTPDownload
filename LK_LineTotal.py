f6eb0a6037b337cf4bcfd69ae1fadc8c67ef3550
LittleKey <LittleKeyRain@gmail.com>
2014-05-04 02:02:57 +0800

    update sprite

--- LK_LineTotal.py
+++ LK_LineTotal.py
@@ -19,7 +19,7 @@ def GetDirList(cdir):
 
     return currentDirs
 
-def CountOneDir(inputDir):
+def CountOneDir(inputDir, lFlag):
     fileList = GetFileList(inputDir)
 
     matchList = list(filter(match.match, fileList))
@@ -34,22 +34,32 @@ def CountOneDir(inputDir):
     for filename in absFileList:
         try:
             with open(filename, encoding='utf-8') as aFile:
-                total += len(list(aFile.readlines()))
+                num = len(list(aFile.readlines()))
+                if lFlag:
+                    print("{num} line in {filename}".format(num=num, filename=filename))
+                total += num
         except UnicodeDecodeError:
-            print("[IOError]: Can't open file '{}'.".format(filename))
-                
+            print("[IOError]: Can't open file '{}'.".format(filename))                
 
     return total
 
 if __name__ == '__main__':
     inputDir = '.'
     rFlag = False # 是否递归的flag
+    lFlag = False # 是否列出被计算的文件
     try:
         num = sys.argv.index('-r')
         del sys.argv[num]
         rFlag = True
     except ValueError:
         rFlage = False
+
+    try:
+        num = sys.argv.index('-l')
+        del sys.argv[num]
+        lFlag = True
+    except ValueError:
+        lFlage = False
 
     if len(sys.argv) == 3:
         inputDir = sys.argv.pop()
@@ -64,13 +74,15 @@ if __name__ == '__main__':
     if not os.path.exists(inputDir):
         print("[FileNotFoundError]: No such file '{}'".format(inputDir))
         exit(0)
+ 
     dirList = [inputDir]
     if rFlag:
 # 递归处理所以子目录(谨慎使用...)
         dirList += GetDirList(inputDir)
+    dirSet = set(dirList) # 使用集合 去掉重复的目录
 
     total = 0
-    for cdir in dirList:
-        total += CountOneDir(cdir)
+    for cdir in dirSet:
+        total += CountOneDir(cdir, lFlag)
 
-    print("Line total is {total}".format(total=total))
+    print("\nLine total is {total}".format(total=total))
