cef5599bc5430301805b5a523f70d7ff62c51263
LittleKey <LittleKeyRain@gmail.com>
2014-06-08 17:22:06 +0000

    update Test main

--- src/new_main.py
+++ src/new_main.py
@@ -3,13 +3,17 @@
 
 from FileList import FileTableFactory
 from FTP import FTPFactory
+import time
 
 def main():
     ftp = FTPFactory().GetFTP()
     factory = FileTableFactory(ftp)
     filetable = factory.New()
 
-    print filetable.GetDirList()
+    while True:
+        time.sleep(10)
+        print(filetable.GetFileList())
+
     for l in factory.Listener:
         l.join()
 
