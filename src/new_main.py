d6f1e7c22727545f7c1ff0d27426540ce1667773
LittleKey <LittleKeyRain@gmail.com>
2014-06-08 18:41:54 +0000

    update

--- src/new_main.py
+++ src/new_main.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from FileList import FileTableFactory
 from FTP import FTPFactory
-import time
+import sys
+from Downloader import NewDownload
 
-def main():
+def main(sysList):
     ftp = FTPFactory().GetFTP()
-    factory = FileTableFactory(ftp)
-    filetable = factory.New()
-
-    while True:
-        time.sleep(10)
-        print(filetable.GetFileList())
-
-    for l in factory.Listener:
-        l.join()
-
+    NewDownload(ftp, *sysList)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
